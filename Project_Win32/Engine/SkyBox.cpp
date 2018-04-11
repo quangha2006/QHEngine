a6dc7e1f041fcdfe0b5a37de012c4dcb30637374
quangha2006 <hdquang206@gmail.com>
2018-04-11 19:36:19 +0700

    implement skybox

--- Project_Win32/Engine/SkyBox.cpp
+++ Project_Win32/Engine/SkyBox.cpp
@@ -1,5 +1,5 @@
 #include "SkyBox.h"
-
+#include <SOIL.h>
 
 
 void SkyBox::Init(const char * texturepath)
@@ -34,6 +34,50 @@ void SkyBox::Init(const char * texturepath)
 
 	mShader.createProgram(verShader, fragShader, true);
 
+	std::vector<std::string> faces
+	{
+		"right.jpg",
+		"left.jpg",
+		"top.jpg",
+		"bottom.jpg",
+		"front.jpg",
+		"back.jpg"
+	};
+
+	cubemapTexture = loadCubemap(faces);
+
+}
+
+unsigned int SkyBox::loadCubemap(std::vector<std::string> faces)
+{
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+	int width, height, nrChannels;
+	for (unsigned int i = 0; i < faces.size(); i++)
+	{
+		unsigned char *data = SOIL_load_image(faces[i].c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+				0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+			);
+			SOIL_free_image_data(data);
+		}
+		else
+		{
+			std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+			SOIL_free_image_data(data);
+		}
+	}
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+	return textureID;
 }
 
 SkyBox::SkyBox()
--- Project_Win32/Engine/SkyBox.h
+++ Project_Win32/Engine/SkyBox.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "Shader.h"
+#include <vector>
+
 class SkyBox
 {
 private:
 	Shader mShader;
+	unsigned int textureID;
+	unsigned int cubemapTexture;
 public:
 	void Init(const char * texturepath);
+	unsigned int loadCubemap(std::vector<std::string> faces);
 	SkyBox();
 	~SkyBox();
 };
