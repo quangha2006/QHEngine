b0bf6cbbc684dc44f8b93a3b85dc9fc0d7626526
Ha Duy Quang <quang.haduy@gameloft.com>
2018-11-30 17:44:13 +0700

    update

--- Project_Win32/Engine/Model.cpp
+++ Project_Win32/Engine/Model.cpp
@@ -113,10 +113,10 @@ void Model::Init(string const & path, Camera *camera, bool FlipUVs, bool enableA
 		LOGI("NumAnimation: %d\n", mNumAnimations);
 	}
 	LOGI("Mesh Count: %d\n", m_pScene->mNumMeshes);
-	//for (int i = 0; i < meshes.size(); i++)
-	//{
-	//	//LOGI("  %d: %s\n", i, meshes[i].GetName());
-	//}
+	for (int i = 0; i < meshes.size(); i++)
+	{
+		LOGI("  %d: %s\n", i, meshes[i].GetName().c_str());
+	}
 
 	isModelLoaded = true;
 	
@@ -498,6 +498,15 @@ void Model::UpdateSkeleton(int64_t time)
 
 	if (time != -1)
 		RunningTime = time;
+	//for (int i = 0; i < Transforms.size(); i++)
+	//{
+	//	glm::mat4 abc = Transforms[i];
+		//LOGI("\n");
+		//LOGI("\n%f, %f, %f, %f", abc[0][0], abc[0][1], abc[0][2], abc[0][3]);
+		//LOGI("\n%f, %f, %f, %f", abc[1][0], abc[1][1], abc[1][2], abc[1][3]);
+		//LOGI("\n%f, %f, %f, %f", abc[2][0], abc[2][1], abc[2][2], abc[2][3]);
+		//LOGI("\n%f, %f, %f, %f", abc[3][0], abc[3][1], abc[3][2], abc[3][3]);
+	//}
 	BoneTransform(RunningTime, Transforms);
 }
 void Model::BoneTransform(float TimeInSeconds, vector<glm::mat4>& Transforms)
