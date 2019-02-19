f6ddc91b503158f67853453edf662fa553b293fb
Ha Duy Quang <quang.haduy@gameloft.com>
2019-02-19 16:18:02 +0700

    update assimp for android

--- Project_Win32/External/AssimpLib/code/res/filename.py
+++
@@ -1,6 +0,0 @@
-import fnmatch
-import os
-
-for file in os.listdir('../'):
-    if fnmatch.fnmatch(file, '*.cpp'):
-        print "\t\t\"$ExternalLibs_Path$/AsimpLib/code/" + file + "\""
--- Project_Win32/External/AssimpLib/remake_assimp.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM pushd .\assimp
-if exist "CMakeCache.txt" del /Q "CMakeCache.txt"
-call cmake -G "Visual Studio 14 2015"
-REM popd
-pause
