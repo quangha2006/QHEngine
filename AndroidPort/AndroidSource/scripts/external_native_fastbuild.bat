b993c8eb8a85b86c8b369a8e669f3eb334399e91
quang.haduy@gameloft.com <quang.haduy@gameloft.com>
2020-06-23 11:05:41 +0700

    Update fastbuild script

--- AndroidPort/AndroidSource/scripts/external_native_fastbuild.bat
+++ AndroidPort/AndroidSource/scripts/external_native_fastbuild.bat
@@ -8,7 +8,7 @@ set FAST_BUILD_CONFIG=!ROOT_DIR!\AndroidSource\scripts\bff
 set FAST_BUILD_COMPILE=!FAST_BUILD_CONFIG!\Compile
 set FAST_BUILD_GENERATED=!ROOT_DIR!\generated
 set FAST_BUILD_LINK=!FAST_BUILD_CONFIG!\Link
-set FAST_BUILD_TOOL=%ROOT_DIR%\..\Tools\FASTBuild-Windows-x64-v0.96\FBuild.exe
+set FAST_BUILD_TOOL=%ROOT_DIR%\..\Tools\FastBuild\bin\FBuild.exe
 set SO_LIB_FILE=native-activity
 
 rem uncomment the line below to see the entire command used on this .bat script
@@ -18,7 +18,7 @@ set architectures=armeabi-v7a
 set architecture=ARM
 set jniLibs_path=!FAST_BUILD_CONFIG!\..\..\app\libs
 
-set FASTBUILD_BROKERAGE_PATH=\\sa2bld0010\FastBuild_Detection
+set FASTBUILD_BROKERAGE_PATH=\\gameloft.org\sai\FastBuild_Detection
 set DO_STATIC_LIBS_COMPARE=FALSE
 set DO_CLEAN_LINK=FALSE
 
