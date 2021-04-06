973aad38afc6c7239eaf46f2809764d44298ede1
quang.haduy@gameloft.com <quang.haduy@gameloft.com>
2021-04-06 15:42:13 +0700

    Config zlib

--- Project_Win32/External/assimp-5.0.1/revision.h
+++ Project_Win32/External/assimp-5.0.1/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0xa6334f28
+#define GitVersion 0x8933c9a9
 #define GitBranch "master"
 
 #define VER_MAJOR 5
@@ -16,13 +16,13 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit a6334f28)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 8933c9a9)"
 #endif
 
 #ifdef  NDEBUG
-#define VER_ORIGINAL_FILENAME_STR   "assimp.dll"
+#define VER_ORIGINAL_FILENAME_STR   "assimp-vc141-mt.dll"
 #else
-#define VER_ORIGINAL_FILENAME_STR   "assimp.dll"
+#define VER_ORIGINAL_FILENAME_STR   "assimp-vc141-mtd.dll"
 #endif //  NDEBUG
 
 #endif // ASSIMP_REVISION_H_INC
